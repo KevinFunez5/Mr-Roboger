de48e84fcd77708976983d85682b9eadd856cbb2
Kevin Funez <kevin.funez315@gmail.com>
2021-06-18 13:25:29 -0700

    function if 1 is entered it returns beep

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function processNumber(number){
+  if (!number){
+    return [];
+  }
+}
+const result = [];
+number = parseInt(number);
+for(let i = 0; i <= number; i ++){
+  let finalValue = i.toString();
+}
+if (i.toString().inculdes("1")){
+  finalValue = "Beep!"
+}
