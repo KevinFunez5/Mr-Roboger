f399071ab4d5e1b9e75c8cd20e1eb20de47f2064
Kevin Funez <kevin.funez315@gmail.com>
2021-06-18 13:49:33 -0700

    2 and 3 function

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
 function processNumber(number){
   if (!number){
     return [];
-  }
-}
-const result = [];
-number = parseInt(number);
-for(let i = 0; i <= number; i ++){
-  let finalValue = i.toString();
-}
-if (i.toString().inculdes("1")){
-  finalValue = "Beep!"
-}
+    
+  }}
+
+  const result = [];
+  number = parseInt(number);
+  for(let i = 0; i <= number; i++){
+    let finalValue = i.toString();
+    if (i.toString().includes('1')) {
+      finalValue = "Beep!";
+    }
+    if (i.toString().includes('2')) {
+      finalValue = "Boop!"
+    }
+    if (i.toString().includes('3')){
+      finalValue = "Won't you be my neighbor?";
+    }
+    result.push(finalValue);
+  } 
+  return result;
